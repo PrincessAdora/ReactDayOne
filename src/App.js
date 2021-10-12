b5b9197939f20a2d86a41bd5846130c3004e7d47
PrincessAdora <89851664+PrincessAdora@users.noreply.github.com>
2021-10-12 12:31:53 -0400

    Changes to App.js Day 1 10/12 CS + The City

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="catraandglimmer.jpg" alt="catra kissing glimmer" />
+        <h1>About Chris Hernandez</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Call me Chris <br></br>
+          Favorite quote:{" "}
+          <i>
+            "It's hard, keeping your heart open. It makes you vulnerable, but it
+            doesn't make you weak...And I have to believe that it's worth it."
+          </i>
         </p>
         <a
           className="App-link"
